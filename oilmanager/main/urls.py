27fe3f3d0db8dcabcd6065adacd4b707798832f9
Davide-Pinto <114738638+Davide-Pinto@users.noreply.github.com>
2023-12-24 16:02:49 +0000

    UI updates
    UI client list (not finish)

--- oilmanager/main/urls.py
+++ oilmanager/main/urls.py
@@ -3,5 +3,10 @@ from . import views  # Import the views module from your app
 
 urlpatterns = [
     path('', views.home, name='home'),  # Set the root URL to use the home view
+    path('clients/', views.clients, name='clients'),
+    path('lagares/', views.lagares, name='lagares'),
+    path('loads/', views.loads, name='loads'),
+    path('stats/', views.stats, name='stats'),
+    path('dashboard/', views.dashboard, name='dashboard'), 
     # ... other url patterns ...
 ]
--- oilmanager/main/views.py
+++ oilmanager/main/views.py
@@ -4,3 +4,18 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'dashboard.html')
+
+def clients(request):
+    return render(request, 'client_list.html')
+
+def lagares(request):
+    return render(request, 'lagares.html')
+
+def loads(request):
+    return render(request, 'loads.html')
+
+def stats(request):
+    return render(request, 'stats.html')
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
