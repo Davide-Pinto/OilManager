d3523c41e55a841f637619de85b597c392ef0308
Davide-Pinto <114738638+Davide-Pinto@users.noreply.github.com>
2023-12-30 00:48:49 +0000

    popup

---
+++ oilmanager/main/static/js/popup.js
@@ -0,0 +1,7 @@
+function openPopup() {
+    document.getElementById("popupForm").style.display = "block";
+}
+
+function closePopup() {
+    document.getElementById("popupForm").style.display = "none";
+}
